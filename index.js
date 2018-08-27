68d05738dc35d79c31521ea0b270908a1abd04b7
Jimmy Sanders-Cannon <jimmysancan@yahoo.com>
2018-08-27 16:09:29 -0400

    webscrape & transaction data

---
+++ index.js
@@ -0,0 +1,29 @@
+const xml = new XMLHttpRequest();
+const url = './transactions.json';
+
+xml.open('GET', url, true)
+xml.send();
+xml.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    let data = JSON.parse(this.responseText);
+    createRows(data);
+  }
+};
+const tableHeader = 
+  `<thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Name</th>
+      <th scope="col">Total Transactions</th>
+      <th scope="col">Total Volume</th>
+      <th scope="col">24h Volume</th>
+      <th scope="col">Volume by Currency</th>
+    </tr>
+  </thead>`;
+
+function createRows(array) {
+  let html = array.map((item) => {
+    return `<tr class='Number'><td>${item.Number}</td><td class="Name">${item.Name}</td><td class="totalTransactions">${item[`Total Transactions`]}</td></tr>`
+  })
+  document.getElementById('mainTable').innerHTML = tableHeader + html.join('')
+}
